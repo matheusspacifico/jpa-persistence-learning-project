40180ab79319e9ef86eb59a5396bc843fa4a7ad9
matheusspacifico <matheuspacifico44@gmail.com>
2025-02-26 15:52:02 -0300

    feat: Aluno class and entity factory test

--- src/main/java/br/edu/ifsp/matheuspacifico/Main.java
+++ src/main/java/br/edu/ifsp/matheuspacifico/Main.java
@@ -1,7 +1,29 @@
 package br.edu.ifsp.matheuspacifico;
 
+import br.edu.ifsp.matheuspacifico.model.Aluno;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+import java.math.BigDecimal;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Aluno aluno1 = new Aluno();
+        aluno1.setNome("Asdrubal da Silva");
+        aluno1.setRa("1234567");
+        aluno1.setEmail("asdrubal.silva@gmail.com");
+        aluno1.setNota1(BigDecimal.valueOf(7.0));
+        aluno1.setNota2(BigDecimal.valueOf(6.0));
+        aluno1.setNota3(BigDecimal.valueOf(8.0));
+
+        EntityManagerFactory factory = Persistence.createEntityManagerFactory("avaliacao1prw3");
+        EntityManager em = factory.createEntityManager();
+
+        em.getTransaction().begin();
+        em.persist(aluno1);
+        em.getTransaction().commit();
+
+        em.close();
     }
 }
